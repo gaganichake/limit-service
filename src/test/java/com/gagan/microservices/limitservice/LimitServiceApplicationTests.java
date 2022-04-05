df39e7dd359114739177c44203870ee4ac04ff07
Gagan Ichake <gaganichake@yahoo.com>
2022-04-05 07:27:23 -0500

    Initial Project setup

---
+++ src/test/java/com/gagan/microservices/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gagan.microservices.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
