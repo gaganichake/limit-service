2cbd2d8284031d9270749dc533a3964a659befad
Gagan Ichake <gaganichake@yahoo.com>
2022-04-05 07:51:43 -0500

    Enhance limits service - Get configuration from properties file

--- src/main/java/com/gagan/microservices/limitservice/controller/LimitsController.java
+++ src/main/java/com/gagan/microservices/limitservice/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.gagan.microservices.limitservice.controller;
 
 import com.gagan.microservices.limitservice.bean.Limits;
+import com.gagan.microservices.limitservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retriveLimits() {
-        return new Limits(1, 10000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
